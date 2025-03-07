1cc2fed555642a3cee6cafd54a8eac7716993327
teriTheDesigner <tere1109@stud.kea.dk>
2025-03-07 14:49:09 +0100

    Import components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,69 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import { Input, InputField } from "@/components/ui/input";
+import { Button, ButtonText, ButtonIcon } from "@/components/ui/button";
+import React from "react";
+import { CategoriesEntity } from "../CategoriesEntity";
 
 export default function HomeScreen() {
+  const [CategoriesScreens, setCategoriesScreens] = React.useState(
+    [] as CategoriesEntity[]
+  );
+  const [CategoriesScreen, setCategoriesScreen] = React.useState("");
+  const onAddCategoriesScreen = () => {
+    const newCategoriesScreen = new CategoriesEntity(
+      CategoriesScreens.length,
+      CategoriesScreen
+    );
+    setCategoriesScreens([...CategoriesScreens, newCategoriesScreen]);
+    console.log(CategoriesScreens);
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <Input>
+        <InputField
+          placeholder="Enter text here..."
+          onChangeText={setCategoriesScreen}
+          value={CategoriesScreen}
+          className="color-white w-56"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </Input>
+
+      <Button>
+        <ButtonText
+          onPress={onAddCategoriesScreen}
+          className="font-medium text-sm ml-2"
+        >
+          Add CategoriesScreen
+        </ButtonText>
+        <ButtonIcon />
+      </Button>
+
+      <FlatList
+        data={CategoriesScreens}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text style={styles.itemText}>{item.title}</Text>
+          </View>
+        )}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  listItem: {
+    backgroundColor: "#e0e0e0",
+    padding: 10,
+    marginTop: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  listText: {
+    fontSize: 16,
   },
+  itemText: { fontSize: 16, width: 200 },
 });
---
+++ app/CategoriesEntity.ts
@@ -0,0 +1,5 @@
+export class CategoriesEntity {
+  public completed: boolean = false;
+
+  constructor(public id: number, public title: string) {}
+}
